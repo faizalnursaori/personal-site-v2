a7e34764042b155be09bf7ba2ee7a843d5458a59
Faizal Nursaori <faizalrahmatn@gmail.com>
2025-02-04 01:07:34 +0700

    fix: link

--- src/utils/NavLinks.ts
+++ src/utils/NavLinks.ts
@@ -2,7 +2,7 @@ export const navLinks = [
   { href: "/about", label: "About" },
   { href: "/projects", label: "Projects" },
   {
-    href: "https://drive.usercontent.google.com/download?id=1WGA1vqAHCno0nVluyxUsP9tGTfcAuukS&export=download&authuser=0&confirm=t&uuid=96fb85ee-bf19-4e10-9b76-5b78975072b8&at=AIrpjvPYH4ZEpw3s_fHQ2Px1Sqiv:1738598064603",
+    href: "https://drive.google.com/file/d/1GAXhQshzr1XK9uqA-bpo7NsA2QyyZ5Hg/view?usp=drive_link",
     label: "My CV",
   },
   { href: "mailto:someone@faizalrahmatn@gmail.com", label: "Contact Me", mobileOnly: true },
